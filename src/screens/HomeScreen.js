3359bb8e757a98dd8a8f7a32f80bc159fde627c1
Jayden Pereira <jayden.pereira@yahoo.com>
2021-03-29 12:09:07 -0400

    Add preliminary agenda on home

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { View, Text, StyleSheet, SafeAreaView } from 'react-native';
+import { Agenda } from 'react-native-calendars';
 
 export default class HomeScreen extends React.Component {
 	constructor(props) {
@@ -15,7 +16,26 @@ export default class HomeScreen extends React.Component {
 				<Text style={styles.header}>
 					Hello, <Text style={styles.headerName}>{this.state.userName}</Text>
 				</Text>
-				<View style={styles.mainContent}></View>
+				<View style={styles.mainContent}>
+					<Agenda
+						items={{
+							'2021-03-29': [{ name: 'item 1 - any js object' }],
+							'2021-04-05': [{ name: 'item 2 - any js object', height: 80 }],
+							'2021-04-07': [],
+							'2021-04-15': [{ name: 'item 3 - any js object' }, { name: 'any js object' }],
+						}}
+						theme={{
+							dayTextColor: '#1a1b1b',
+							todayTextColor: '#4d97b6',
+							dotColor: '#1a1b1b',
+							backgroundColor: '#f4f4f4',
+							selectedDayBackgroundColor: '#a9d5e6',
+							agendaDayTextColor: '#a9d5e6',
+							agendaDayNumColor: '#a9d5e6',
+							agendaTodayColor: '#4d97b6',
+						}}
+					/>
+				</View>
 			</SafeAreaView>
 		);
 	}
@@ -43,7 +63,6 @@ const styles = StyleSheet.create({
 	},
 	mainContent: {
 		flex: 1,
-		marginHorizontal: 16,
 		backgroundColor: 'red',
 	},
 });
