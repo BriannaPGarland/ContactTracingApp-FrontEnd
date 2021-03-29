c83ddcdfa6c84e96b44fd92ff715f185367dfab8
Jayden Pereira <jayden.pereira@yahoo.com>
2021-03-28 20:52:27 -0400

    Add basic app initialization workflow

--- App.js
+++ App.js
@@ -10,15 +10,42 @@ import FriendScreen from './src/screens/FriendScreen';
 const Tab = createBottomTabNavigator();
 
 export default class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			isInitialized: false,
+			loggedIn: false,
+		};
+	}
+
+	componentDidMount() {
+		// Force account and app initialization for now
+		this.setState({
+			isInitialized: true,
+			loggedIn: true,
+		});
+	}
+
 	render() {
-		return (
-			<NavigationContainer>
-				<Tab.Navigator initialRouteName='Home'>
-					<Tab.Screen name='Friend' component={FriendScreen} />
-					<Tab.Screen name='Home' component={HomeScreen} />
-					<Tab.Screen name='Account' component={AccountScreen} />
-				</Tab.Navigator>
-			</NavigationContainer>
-		);
+		const { isInitialized, loggedIn } = this.state;
+		if (isInitialized) {
+			if (!loggedIn) {
+				// Signup and Login workflow
+				return <View></View>;
+			} else {
+				return (
+					<NavigationContainer>
+						<Tab.Navigator initialRouteName='Home'>
+							<Tab.Screen name='Friend' component={FriendScreen} />
+							<Tab.Screen name='Home' component={HomeScreen} />
+							<Tab.Screen name='Account' component={AccountScreen} />
+						</Tab.Navigator>
+					</NavigationContainer>
+				);
+			}
+		} else {
+			// Splash Screen
+			return <View></View>;
+		}
 	}
 }
